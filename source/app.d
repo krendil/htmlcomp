97ded4df5b099551febc81cf3893dda63031f7df
David Osborne <krendil@localhost.localdomain>
2014-03-13 00:45:25 +1000

    Removed non-working code

--- source/app.d
+++ source/app.d
@@ -6,11 +6,11 @@ import std.range;
 
 immutable source = import("getting-started-with-microformats2.htm");
 
-immutable libs = ["Arsd", "Gumbo-d", "KXML", "Tango"];
+immutable libs = ["Arsd", "Gumbo-d"];
 
 void main()
 {
-    auto results = benchmark!(runArsd, runGumbo, runKXML, runTango)(10000);
+    auto results = benchmark!(runArsd, runGumbo)(10000);
 
     foreach( lib, time; libs[].zip(results[]) ) {
         writefln( "%s:\t%.3f s", lib, time.to!("seconds", double) );
@@ -24,15 +24,5 @@ void runArsd() {
 
 void runGumbo() {
     import htmlcomp.gumbo;
-    auto e = run(source);
-}
-
-void runKXML() {
-    import htmlcomp.kxml;
-    auto e = run(source);
-}
-
-void runTango() {
-    import htmlcomp.tango;
     auto e = run(source);
 }
